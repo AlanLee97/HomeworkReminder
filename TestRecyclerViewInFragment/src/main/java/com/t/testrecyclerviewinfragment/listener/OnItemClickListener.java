aa3774c00443c5818c9fd2902f737554adeabc4f
李步官 <42601044+AlanLee97@users.noreply.github.com>
2019-09-16 08:28:27 +0800

    删除了一些测试文件

--- TestRecyclerViewInFragment/src/main/java/com/t/testrecyclerviewinfragment/listener/OnItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.t.testrecyclerviewinfragment.listener;
-
-import android.view.View;
-
-public interface OnItemClickListener {
-    public void onItemClickListener(View view, int position);
-}
